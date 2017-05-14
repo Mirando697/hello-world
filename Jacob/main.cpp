88b33ade92f48c40964df47445d4cf44acde79a5
Jakub Dziewonski <Jakub Dziewonski>
2017-05-14 13:12:48 +0200

    zmiany jezykowe

--- Jacob/main.cpp
+++ Jacob/main.cpp
@@ -5,10 +5,20 @@ using namespace std;
 
 int main()
 {
-    Event Easter_Egg1;
-
-    Easter_Egg1.show();
-    Easter_Egg1.choice();
+    Event easterEgg;
+	//lepiej wygląda i działa w tym przypadku easterEgg1
+	//niżej masz wyjaśnienie
+	
+    easterEgg.ShowResult();
+    easterEgg.ChooseAction();
 
     return 0;
 }
+
+//pierwsza rzecz, to takie powiedzmy zasady językowe ogólnie przyjęte w programowaniu 
+//nazwy klas zaczynasz z dużej czyli u ciebie jest dobrze Event 
+//zmienne w klasie zaczynasz z małej a potem kolejne słowo w zmiennej z dużej czyli 
+//eggsCount, ultraMegaClassMenager, isPlayerActive
+//nazwy funkcji zaczynasz z dużej np. 
+// GetEnemysCount(){} , SetEnemeysCount(){}, InstantiateEnemy(), CreateEnemy
+// to taka pierwsza rzecz która pomimo tego że nie znam języka rzuca mi się w oczy 
